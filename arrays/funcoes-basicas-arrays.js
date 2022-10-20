fc1599668b9d9b0349cad5121c476b8a47a040d7
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-20 17:52:23 -0300

    Adicionando slice e splice

--- arrays/funcoes-basicas-arrays.js
+++ arrays/funcoes-basicas-arrays.js
@@ -34,3 +34,27 @@ console.log(posicaoPorsche)
 let posicaoBugatti = marcasCarros.indexOf("Bugatti")
 console.log(posicaoBugatti)
 //Quando nao encontra o valor na array é retornado o valor de -1
+
+//Splice significa emendar ou costurar. Ele substitui o array original
+//No splice so o primeiro parametro é obrigatorio
+//Sintaxe: array.splice(index[, deleteCount[, elemento1[, ...[, elementoN]]])
+console.log(marcasCarros)
+let carrosRemovidos = marcasCarros.splice(1, 2, "Ferrari", "Lamborghini", "Rolls Royce")
+//Vai remover o que esta na posicao 1 e 2 e colocar no lugar os elementos a seguir
+//Ele tambem retorna os elementos removidos
+console.log(marcasCarros)
+console.log(carrosRemovidos)
+
+//Se nao passarmos nenhum elemento, ele ira remover os elementos indicados. Ex: no caso, o elemento 3
+let elementoRemovido = marcasCarros.splice(2, 1)
+//A partir da posicao 2 ele só ira remover 1 elemento
+console.log(marcasCarros)
+console.log(elementoRemovido)
+
+//O slice vai alterar a ordem do array sem alterar o original. Significa fatiar ou dividir
+//Sintaxe: lista.slice([indice-inicial[, indice-final]])
+//OBS: nao sera incluido o indice-final
+let carrosExtraidos = marcasCarros.slice(1, 3)
+console.log(marcasCarros)
+console.log(carrosExtraidos)
+//Criou um novo array com os valores que foram extraidos
