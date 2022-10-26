76610c88149e7eac3381a7079990f96dd8d78a14
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-26 17:52:14 -0300

    Create intro.js

---
+++ object-oriented-programming/intro.js
@@ -0,0 +1,29 @@
+/*
+POO - Programacao Orientada a Objetos
+A base sao as classes e os objetos. Todo objeto é criado a partir de uma classe
+A classe é como se fosse um molde onde sao definidos os atributos e os metodos que um objeto vai receber
+*/
+
+class Carro {
+}
+// OBS: Em uma classe a primeira letra deve ser maiuscula
+
+let landRover = new Carro()
+// Abrimos e fechamos parenteses assim como quando criamos uma funcao
+
+console.log(landRover)
+// landRover é um objeto da classe Carro
+
+/*
+Anteriormente aprendi sobre Array que é uma classe do JavaScript
+Os arrays possuem funcoes (forEach, filter, find, pop, findIndex) que sao metodos internos da classe array 
+  Codar:
+let lista = []
+  É o mesmo que:
+let lista = new Array()
+
+O mesmo acontece com o Object (dicionario em python).
+Todos os tipos possuem uma classe (String, Number, Boolean)
+E com POO podemos criar as nossas proprias classes
+*/
+
