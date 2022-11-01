7ccc8edb1c5903c1adbac625549d047006148cc6
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-11-01 17:55:19 -0300

    Create script.js

---
+++ object-oriented-programming/exercicio/script.js
@@ -0,0 +1,28 @@
+/*
+nome, titulos, federacao, time
+se nba qual numero da camisa deseja 
+se nbb qual salario recebe
+
+lances livres da carreira deve iniciar com 0
+um menu deve apresentar:
+1- treinar lances livres - qtd
+2- trocar jogador
+3- imprimir quantos lances livres foram chutados e sair
+
+para treinar lances livres deve ser pedido a quantidade de chutes
+
+Se for cadastrar outro jogador (2), todos os dados de cadastro serão solicitados novamente e a quantidade
+de arremessos do jogador deve iniciar com 0
+
+A impressão deve ser:
+
+Nome: <nome>
+Qtd de arremessos: <quantidade>
+Numero da camisa: <numero>
+Salario: <salario>
+
+
+OBS: cada classe declarada deve ficar num arquivo à parte. O arquivo principal deve conter apenas a 
+inicialização da aplicação e a chamada para as outras classes
+*/
+
